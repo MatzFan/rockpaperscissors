0f3ea44dede62c1742cdd615a37d960424fad76a
bruce / simon <bruce.steedman@gmail.com , simon@simonwoolf.net>
2014-01-06 16:13:37 +0000

    supports outcome string

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -11,22 +11,34 @@ Player.prototype.picks = function(pick) {
 function Game(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
-
-  this.pairs = {'paper':['rock','spock'],
-                'rock':['scissors', 'lizard'],
-                'lizard':['spock', 'paper'],
-                'spock':['scissors', 'rock'],
-                'scissors':['lizard', 'paper']};
 }
+
+Game.prototype.pairs = {'paper':{'rock':'covers', 'spock':'disproves'},
+                        'rock':{'scissors':'crushes', 'lizard':'crushes'},
+                        'lizard':{'spock':'poisons', 'paper':'eats'},
+                        'spock':{'scissors':'smashes', 'rock':'vaporizes'},
+                        'scissors':{'lizard': 'decapitate', 'paper':'cut'}};
 
 Game.prototype.winner = function() {
   if(this.player1.pick === this.player2.pick) {
     return null
   }
   
-  if(this.pairs[this.player1.pick].indexOf(this.player2.pick) > -1 ) {
+  if(this.pairs[this.player1.pick][this.player2.pick] != null ) {
     return this.player1;
   } else {
     return this.player2;
   }
+}
+
+Game.prototype.loser = function() {
+  return (this.winner() === this.player1) ? this.player2 : this.player1;
+}
+
+
+Game.prototype.outcomeString = function() {
+  var winnerPick = this.winner().pick;
+  var loserPick = this.loser().pick;
+  var verb = this.pairs[winnerPick][loserPick];
+  return [winnerPick,verb,loserPick].join(' ');
 }
--- javascript/spec/RockPaperScissorsSpec.js
+++ javascript/spec/RockPaperScissorsSpec.js
@@ -1,35 +1,55 @@
 describe("Rock-Paper-Scissors", function() {
-  var player1, player2, game;
+  var player1, player2, game, pairs, outcomes;
 
   beforeEach(function() {
 
     player1 = new Player();
     player2 = new Player();
     game = new Game(player1, player2);
-
+    pairs = [['paper', 'rock'],
+             ['paper', 'spock'],
+             ['rock', 'scissors'],
+             ['rock', 'lizard'],
+             ['lizard', 'spock'],
+             ['lizard', 'paper'],
+             ['spock', 'scissors'],
+             ['spock', 'rock'],
+             ['scissors', 'lizard'],
+             ['scissors', 'paper']];
   });
 
   describe('winning and losing', function() {
     it('pairs should win/lose according to the rules', function() {
-      var pairs = [['paper', 'rock'],
-        ['paper', 'spock'],
-        ['rock', 'scissors'],
-        ['rock', 'lizard'],
-        ['lizard', 'spock'],
-        ['lizard', 'paper'],
-        ['spock', 'scissors'],
-        ['spock', 'rock'],
-        ['scissors', 'lizard'],
-        ['scissors', 'paper']];
-        outcomes = pairs.map(function([winner, loser]) {
-          player1.picks(winner);
-          player2.picks(loser);
-          return game.winner();
-        });
-        function isPlayer1(element, index, array) {
-          return (element === player1);
-        };
-        expect(outcomes.every(isPlayer1)).toBe(true);
+      outcomes = pairs.map(function([winner, loser]) {
+        player1.picks(winner);
+        player2.picks(loser);
+        return game.winner();
+      });
+      function isPlayer1(element, index, array) {
+        return (element === player1);
+      };
+      expect(outcomes.every(isPlayer1)).toBe(true);
+    });
+  });
+
+  describe('outcome string', function() {
+    it('should say "winnermove verbs losermove"', function() {
+      outcomeStrings = pairs.map(function([winner, loser]) {
+        player1.picks(winner);
+        player2.picks(loser);
+        return game.outcomeString(); 
+      });
+      outcomes = ["paper covers rock",
+                  "paper disproves spock",
+                  "rock crushes scissors",
+                  "rock crushes lizard",
+                  "lizard poisons spock",
+                  "lizard eats paper",
+                  "spock smashes scissors",
+                  "spock vaporizes rock",
+                  "scissors decapitate lizard",
+                  "scissors cut paper"]
+      expect(outcomeStrings).toEqual(outcomes)
     });
   });
 
