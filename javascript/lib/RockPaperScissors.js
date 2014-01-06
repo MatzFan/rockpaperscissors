8f1b9fef4fb9bc6c4087352abbbe01b1c3cc5565
Simon Woolf <simon@simonwoolf.net>
2014-01-06 11:24:13 +0000

    implemented js rps

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -3,17 +3,33 @@ function Player(name) {
   // console.log("New player created")
 }
 
-Player.prototype.picks = function() {
-  // if(foo === bar) {
-
-  // }
+Player.prototype.picks = function(pick) {
+  //a setter!
+  this.pick = pick;
 }
 
 function Game(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
+
+  this.pairs = new Array();
+  this.pairs["rock"] = "scissors";
+  this.pairs["scissors"] = "paper";
+  this.pairs["paper"] = "rock";
 }
 
 Game.prototype.winner = function() {
-  return this.player1;
+  console.log("p1: " + this.player1.pick)
+  console.log("p1paired: " + this.pairs[this.player1.pick])
+  console.log("p2: " + this.player2.pick)
+  
+  if(this.player1.pick === this.player2.pick) {
+    return null
+  }
+  
+  if(this.pairs[this.player1.pick] === this.player2.pick) {
+    return this.player1;
+  } else {
+    return this.player2;
+  }
 }
