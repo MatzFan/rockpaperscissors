0c33302211ac98065948980728cf0f72572f2aec
bruce / simon <bruce.steedman@gmail.com , simon@simonwoolf.net>
2014-01-06 14:08:00 +0000

    removed log statements

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -20,16 +20,6 @@ function Game(player1, player2) {
 }
 
 Game.prototype.winner = function() {
-  console.log("p1: " + this.player1.pick)
-  console.log("p2: " + this.player2.pick)
-  console.log("checking: " + [this.player1.pick, this.player2.pick])
-  //console.log("pair in question: " + this.pairs[2])
-  //console.log("pairs filtered: " + this.pairs.filter(function(element, index, array) {
-                                //element[0] == this.player1.pick &&
-                                //element[1] == this.player2.pick
-  //}))
-              
-  
   if(this.player1.pick === this.player2.pick) {
     return null
   }
