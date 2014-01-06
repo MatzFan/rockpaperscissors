c86400b87b89bf471980494c5d2771b30f8b6ee8
bruce / simon <bruce.steedman@gmail.com , simon@simonwoolf.net>
2014-01-06 14:13:39 +0000

    fix jasmine missing brace

--- javascript/spec/RockPaperScissorsSpec.js
+++ javascript/spec/RockPaperScissorsSpec.js
@@ -1,6 +1,6 @@
 describe("Rock-Paper-Scissors", function() {
   var player1, player2, game;
-  
+
   beforeEach(function() {
 
     player1 = new Player();
@@ -12,46 +12,36 @@ describe("Rock-Paper-Scissors", function() {
   describe('winning and losing', function() {
     it('pairs should win/lose according to the rules', function() {
       var pairs = [['paper', 'rock'],
-                  ['paper', 'spock'],
-                  ['rock', 'scissors'],
-                  ['rock', 'lizard'],
-                  ['lizard', 'spock'],
-                  ['lizard', 'paper'],
-                  ['spock', 'scissors'],
-                  ['spock', 'rock'],
-                  ['scissors', 'lizard'],
-                  ['scissors', 'paper']];
-      outcomes = pairs.map(function([winner, loser]) {
-        player1.picks(winner);
-        player2.picks(loser);
-        return game.winner();
-      });
-      function isPlayer1(element, index, array) {
-        return (element === player1);
-      };
-      expect(outcomes.every(isPlayer1)).toBe(true);
+        ['paper', 'spock'],
+        ['rock', 'scissors'],
+        ['rock', 'lizard'],
+        ['lizard', 'spock'],
+        ['lizard', 'paper'],
+        ['spock', 'scissors'],
+        ['spock', 'rock'],
+        ['scissors', 'lizard'],
+        ['scissors', 'paper']];
+        outcomes = pairs.map(function([winner, loser]) {
+          player1.picks(winner);
+          player2.picks(loser);
+          return game.winner();
+        });
+        function isPlayer1(element, index, array) {
+          return (element === player1);
+        };
+        expect(outcomes.every(isPlayer1)).toBe(true);
     });
   });
 
 
   describe('draws', function() {
-
-    describe('any identical picks', function() {
-
-      it('should result in no winner', function() {
-
-        var drawGameResults = ['rock', 'paper', 'scissors'].map(function(x) {
-          player1.picks(x);
-          player2.picks(x);
-          return game.winner();
-        });
-
+    it('should result in no winner', function() {
+      var drawGameResults = ['rock', 'paper', 'scissors'].map(function(x) {
+        player1.picks(x);
+        player2.picks(x);
+        return game.winner();
         expect(drawGameResults).toEqual([null, null, null]);
-
       });
-
     });
-
   });
-
 });
